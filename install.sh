c13aa2ec4523ae7c6d3b17bccbebf86a7957990b
AwJHub <76947375+AwJHub@users.noreply.github.com>
2022-12-05 22:33:32 +0330

    4
    4

--- install.sh
+++ install.sh
@@ -106,29 +106,6 @@ install_x-ui() {
     systemctl stop x-ui
     cd /usr/local/
 
-    if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/AwJHub/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}refresh x-ui version failed,it may due to Github API restriction,please try it later${plain}"
-            exit 1
-        fi
-        echo -e "get x-ui latest version succeed: ${last_version}, begin to install..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/AwJHub/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}dowanload x-ui failed,please be sure that your server can access Github ${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/AwJHub/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "begin to install x-ui v$1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}dowanload x-ui v$1 failed,please check the verison exists${plain}"
-            exit 1
-        fi
-    fi
-
     if [[ -e /usr/local/x-ui/ ]]; then
         rm /usr/local/x-ui/ -rf
     fi
